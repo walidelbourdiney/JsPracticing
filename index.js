3bc37427301c672c81e59dddcee570df258c736b
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-13 18:49:52 +0200

    practice logic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 "use strict";
-alert("hi");
+const heading = document.querySelector("h1");
+heading.addEventListener("mouseenter", (e) => {
+  const element = e.target;
+  element.classList.add("loading");
+  setTimeout(() => {
+    element.classList.remove("loading");
+  }, 3000);
+});
+
+const x = Number(prompt("what is your age?"));
+let y = 2025 - x;
+alert(`you were born in ${y}`);
