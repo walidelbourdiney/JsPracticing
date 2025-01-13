e6b4c24153bc26a9ba36875553c625a5ae7471c9
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-13 19:14:45 +0200

    practice functions

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ heading.addEventListener("mouseenter", (e) => {
   }, 3000);
 });
 
-const x = Number(prompt("what is your age?"));
-let y = 2025 - x;
-alert(`you were born in ${y}`);
+// const x = Number(prompt("what is your age?"));
+// let y = 2025 - x;
+// alert(`you were born in ${y}`);
+
+const people = ["Mario", "Luigi", "Ryu", "Shaun", "Chun-Li"];
+people.forEach((person, index) => {
+  console.log(`${index + 1}: Hello, ${person}`);
+});
